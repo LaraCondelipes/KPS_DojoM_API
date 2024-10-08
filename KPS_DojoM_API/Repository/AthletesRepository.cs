9431f20f4f73096a139df723e5edcc3b8bee3064
LaraCondelipes <LaraCondelipes07@gmail.com>
2024-10-08 15:57:15 +0100

    Athletes Repository created

---
+++ KPS_DojoM_API/Repository/AthletesRepository.cs
@@ -0,0 +1,58 @@
+﻿using KPS_DojoM_API.Data;
+using KPS_DojoM_models;
+using Microsoft.EntityFrameworkCore;
+
+namespace KPS_DojoM_API.Repository
+{
+    public class AthletesRepository : IRepository<Athletes>
+    {
+        private readonly KPS_DbContext context;
+
+        public AthletesRepository(KPS_DbContext context)
+        { 
+            this.context = context;
+        }
+
+        IEnumerable<Athletes> IRepository<Athletes>.All()
+        {
+            return context
+                .Athletes
+                .Include(x => x.Categories)
+                .ToList();
+        }
+
+
+        public Athletes? Get(int id)
+        {
+            return context
+                .Athletes
+                .Include(x => x.Categories)
+                .FirstOrDefault(x => x.Id == id);
+        }
+
+        public Athletes Add(Athletes entity)
+        {
+           context.Athletes.Add(entity);
+            context.SaveChanges();
+
+            return entity;
+        }
+
+        public void Update(Athletes entity)
+        {
+            context.Entry(entity).State = EntityState.Modified;
+            context.SaveChanges();
+        }
+
+        public void Delete(int id)
+        {
+            var value = Get(id);
+
+            if (value != null)
+            { 
+                context.Athletes.Remove(value);
+                context.SaveChanges();  
+            }
+        }
+    }
+}
