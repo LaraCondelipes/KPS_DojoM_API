ff335a9d729e22a9d4d504690429c42ffdd1c10c
LaraCondelipes <LaraCondelipes07@gmail.com>
2024-10-10 12:16:24 +0100

    TheResults respository created

---
+++ KPS_DojoM_API/Repository/TheResultsRepository.cs
@@ -0,0 +1,60 @@
+﻿using KPS_DojoM_API.Data;
+using KPS_DojoM_models;
+using Microsoft.EntityFrameworkCore;
+using NuGet.Packaging.Core;
+
+
+namespace KPS_DojoM_API.Repository
+{
+    public class TheResultsRepository : IRepository<TheResults>
+    {
+        public readonly KPS_DbContext context;
+
+        public TheResultsRepository(KPS_DbContext context)
+        {
+            this.context = context;
+        }
+
+        IEnumerable<TheResults> IRepository<TheResults>.All()
+        {
+            return context
+                .TheResults
+                .Include(x=>x.Category)
+                .Include(x=>x.Event)
+                .ToList();
+        }
+
+        public TheResults? GetValue(int id)
+        { 
+            return context
+                .TheResults
+                .Include(x=>x.Category)
+                .Include(x=>x.Event)
+                .FirstOrDefault();
+        }
+
+        public TheResults Add(TheResults value)
+        { 
+            context.TheResults .Add(value);
+            context.SaveChanges();
+
+            return value;
+        }
+
+        public void Update(TheResults value)
+        { 
+            context.Entry(value).State = EntityState.Modified;
+            context.SaveChanges();  
+        }
+        public void Delete(int id)
+        {
+           var value = GetValue(id);
+
+            if (value != null)
+            { 
+                context.TheResults .Remove(value);  
+                context.SaveChanges();
+            }
+        }
+    }
+}
